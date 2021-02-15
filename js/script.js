ea97dd9ff2797dbd91d45b81011aad8bba98a6e6
Mary Kathryn <msmarykathrynholt@gmail.com>
2021-02-15 16:54:16 -0600

    added elements to set rotation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 // first set interval to call every 1000 milliseconds
 // we are going to call the function setClock
 setInterval(setClock, 1000)
-
+//add element 
+cont hourHand = document.querySelector('[data-hour-hand]')
+cont minuteHand = document.querySelector('[data-minute-hand]')
+cont secondHand = document.querySelector('[data-second-hand]')
 //define function setClock that is going to get current date
 function setClock () {
     // going to get current date
@@ -10,4 +13,9 @@ function setClock () {
     const secondsRatio = currentDate.getSeconds() / 60
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
     const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+    // adding data attributes to html for clock rotation//
+
 }
+
+// function to set rotation 
+function setrotation
